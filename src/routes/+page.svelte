abd70bb7fa3a613fefe729147e9106c168313db3
Richard Marcinčák <em1t.dev@proton.me>
2025-05-19 12:19:37 +0200

    Obfuscate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -105,7 +105,7 @@
 </div>
 
 <div class="fixed bottom-4 left-4 z-40 hidden 2xl:block">
-	<p class="text-gray-400 text ibm-plex-mono-regular-italic">// Richard Marcinčák</p>
+	<p class="text-gray-400 text ibm-plex-mono-regular-italic">// Em1t</p>
 	<a href="/#contact" class="text-gray-200 hover:text-sky-400 text-4xl ibm-plex-mono-regular"
 		>{m.getInTouch()}</a
 	>
@@ -144,8 +144,8 @@
 		</div>
 		<div class="flex flex-col sm:flex-row gap-4">
 			<img
-				src="/Riško.jpg"
-				alt="portrait of Richard Marcinčák"
+				src="/Em1t.png"
+				alt="Em1t logo"
 				class="w-fit aspect-square sm:w-96 sm:h-96 rounded"
 			/>
 			<div class="flex flex-col gap-4 justify-center">
@@ -649,42 +649,6 @@
 						</svg>
 						GitHub: <a href="/" class="text-sky-400 hover:text-slate-200 underline">Em1tt</a>
 					</p>
-					<p class="text-slate-200 ibm-plex-mono-semibold flex flex-row gap-1 items-center">
-						<svg
-							class="w-4 h-4"
-							fill="currentColor"
-							height="800px"
-							width="800px"
-							version="1.1"
-							id="Layer_1"
-							xmlns="http://www.w3.org/2000/svg"
-							xmlns:xlink="http://www.w3.org/1999/xlink"
-							viewBox="0 0 310 310"
-							xml:space="preserve"
-						>
-							<g id="XMLID_801_">
-								<path
-									id="XMLID_802_"
-									d="M72.16,99.73H9.927c-2.762,0-5,2.239-5,5v199.928c0,2.762,2.238,5,5,5H72.16c2.762,0,5-2.238,5-5V104.73   C77.16,101.969,74.922,99.73,72.16,99.73z"
-								/>
-								<path
-									id="XMLID_803_"
-									d="M41.066,0.341C18.422,0.341,0,18.743,0,41.362C0,63.991,18.422,82.4,41.066,82.4   c22.626,0,41.033-18.41,41.033-41.038C82.1,18.743,63.692,0.341,41.066,0.341z"
-								/>
-								<path
-									id="XMLID_804_"
-									d="M230.454,94.761c-24.995,0-43.472,10.745-54.679,22.954V104.73c0-2.761-2.238-5-5-5h-59.599   c-2.762,0-5,2.239-5,5v199.928c0,2.762,2.238,5,5,5h62.097c2.762,0,5-2.238,5-5v-98.918c0-33.333,9.054-46.319,32.29-46.319   c25.306,0,27.317,20.818,27.317,48.034v97.204c0,2.762,2.238,5,5,5H305c2.762,0,5-2.238,5-5V194.995   C310,145.43,300.549,94.761,230.454,94.761z"
-								/>
-							</g>
-						</svg>
-						LinkedIn:
-						<a
-							href="https://www.linkedin.com/in/richard-marcincak/"
-							target="_blank"
-							rel="noreferrer noopener"
-							class="text-sky-400 hover:text-slate-200 underline">Richard Marcinčák</a
-						>
-					</p>
 				</div>
 			</div>
 		</div>
@@ -693,7 +657,7 @@
 <footer class="w-full bg-black py-12">
 	<div class="w-full max-w-7xl mx-auto px-8 md:px-20 relative">
 		<p class="ibm-plex-mono-regular-italic text-gray-400 text-center">
-			//© {new Date().getFullYear()} Richard Marcinčák
+			//© {new Date().getFullYear()} Em1t. All rights reserved.
 		</p>
 	</div>
 </footer>
